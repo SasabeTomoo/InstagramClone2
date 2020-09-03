f4ccf419ba1489ea891366606728706d004d1f46
tomoo sasaabe <sasa.sakurajima@gmail.com>
2020-09-04 08:02:18 +0900

    usersのcontrollerとviewのファクタリング（パーシャル化）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   def new
     @user = User.new
   end
@@ -13,14 +14,21 @@ class UsersController < ApplicationController
     end
   end
   def show
-    #パラメータが飛ぶところ、params[:id]をもとに、そのidに紐づくデータをデータベースから取得
-    @user = User.find(params[:id])
+    #パラメータが飛ぶところ、params[:id]をもとに、そのidに紐づく一つのデータをデータベースから取得
+    # その一つのデータを変数として定義し、それぞれのshow・edit・updateアクションのviewに渡す。
+    # @user = User.find(params[:id])
   end
+  # editアクションは編集する内容を入力させるアクション
   def edit
-    @user = User.find(params[:id])
+    # editへのhttpリクエスト（edit画面へのリンク）が押された時のパラメータ（各idの指定あり）で、編集するデータ（データベース内）を特定し、変数に渡す。
+    # @user = User.find(params[:id])
   end
+  # updateアクションは編集された内容で、データを更新する。機能的には、createと同じ位置付け。アクションがhttp入力で起こるのではなく、form_withのボタンで起こる。
   def update
-    @user = User.find(params[:id])
+    # 一行目のparamsで飛んでいる値は、データベース内（Userモデルを使用して作成されたインスタンス）から、取り出した値。取り出し方は、urlのリクエストをリンクなどで押した覚えはないので、editのform_withに入力したパラメーターが飛んでいると思う（パラメーターの中の入力値は異なるが、idは同じなので、そのidだけを使いデータベースから更新対象のデータを特定していると思う）。
+    # updateアクションの時は、その前に、変数でupdateの対象を特定しないといけない。
+    # @user = User.find(params[:id])
+    # editのform_withフォームで入力した値のパラメーターが、user_paramsに入っている。その値を変数を挟んで、データベースに登録する。
     if @user.update(user_params)
     # idごとのuserのshowページには、下記urlの引数として、そのidが必要
       redirect_to user_path(@user.id), notice: "プロフィールを編集しました！"
@@ -31,5 +39,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
+  end
+  # idをキーとして値を取得するメソッドを追加
+  def set_user
+    @user = User.find(params[:id])
   end
 end
